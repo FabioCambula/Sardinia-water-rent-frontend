49c826cdba234cdc22d55649d02ac96a7e3e31cc
FabioCambula <fabiocambulaa@yahoo.com>
2025-10-21 10:38:14 +0200

    fix axios

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,7 @@ import { useAuthStore } from './stores/auth-stores'
 // Crea un'istanza di Axios
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
+  withCredentials: true,
   headers: {
     'Content-Type': 'application/json',
   },
