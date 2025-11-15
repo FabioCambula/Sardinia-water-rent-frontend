0edf30b68e0e4fa9379e18dbe09fbb5ddda6dcee
FabioCambula <fabiocambulaa@yahoo.com>
2025-11-15 15:12:46 +0100

    change img position

--- src/data/products.js
+++ src/data/products.js
@@ -3,7 +3,7 @@ export const products = [
     id: 'diving-mask',
     name: 'MASCHERA SUB',
     price: 6,
-    img: '@/assets/img/products/maschera.jpg',
+    img: '/products/maschera.jpg',
     description:
       'Permette di esplorare i fondali con comfort e visibilità panoramica. Perfetta per chi vuole scoprire la bellezza del mare senza attrezzature complesse.',
     productId: '68d3f201ccd38cdf9c2d5064',
@@ -12,7 +12,7 @@ export const products = [
     id: 'kayak-1',
     name: 'KAYAK 1 POSTO',
     price: 18,
-    img: '@/assets/img/products/kayak-1.jpg',
+    img: '/products/kayak-1.jpg',
     description:
       'Leggero e maneggevole, ideale per chi vuole vivere il mare in autonomia. Perfetto per brevi escursioni o uscite sportive in solitaria.',
     productId: '68d3f0cbccd38cdf9c2d5055',
@@ -21,7 +21,7 @@ export const products = [
     id: 'kayak-2',
     name: 'KAYAK 2 POSTI',
     price: 22,
-    img: '@/assets/img/products/kayak.jpg',
+    img: '/products/kayak.jpg',
     description:
       'Pensato per coppie o amici, offre stabilità e divertimento condiviso. Ottimo per vivere il mare in compagnia senza rinunciare al comfort.',
     productId: '68d3f19eccd38cdf9c2d5058',
@@ -30,7 +30,7 @@ export const products = [
     id: 'kayak-3',
     name: 'KAYAK 3 POSTI',
     price: 24,
-    img: '@/assets/img/products/kayak-3.jpg',
+    img: '/products/kayak-3.jpg',
     description:
       'Spazioso e sicuro, adatto a famiglie o gruppi di amici. Consente di pagaiare insieme e godersi il mare in totale relax.',
     productId: '68d3f1b5ccd38cdf9c2d505b',
@@ -39,7 +39,7 @@ export const products = [
     id: 'surf',
     name: 'TAVOLA SURF',
     price: 18,
-    img: '@/assets/img/products/surf.jpg',
+    img: '/products/surf.jpg',
     description:
       'Perfetta per cavalcare le onde e mettersi alla prova. Adatta a principianti e intermedi che vogliono avvicinarsi al surf in sicurezza.',
     productId: '68d3f1c1ccd38cdf9c2d505e',
@@ -48,7 +48,7 @@ export const products = [
     id: 'sup',
     name: 'STAND UP PADDLE',
     price: 18,
-    img: '@/assets/img/products/sup.jpg',
+    img: '/products/sup.jpg',
     description:
       'Stabile e versatile, ideale sia per brevi passeggiate in mare che per allenamenti all’aria aperta. Un modo semplice per rilassarsi a contatto con l’acqua.',
     productId: '68d3f1d2ccd38cdf9c2d5061',
